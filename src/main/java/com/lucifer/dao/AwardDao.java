87772d1cd2660a92db664fdce0d2b3b65ba6b550
liufx <liufeng45gh@163.com>
2019-12-16 20:44:41 +0800

    process

--- src/main/java/com/lucifer/dao/AwardDao.java
+++ src/main/java/com/lucifer/dao/AwardDao.java
@@ -2,9 +2,11 @@ package com.lucifer.dao;
 
 import com.lucifer.dao.cms.IBatisBaseDao;
 import com.lucifer.model.MemberAward;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public class AwardDao  extends IBatisBaseDao {
 
     public List<MemberAward> getMemberAwardCount(Long memberId){

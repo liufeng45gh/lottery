924b2f6ab1fa082e6567bff5bd63fe36ee905435
liufx <liufeng45gh@163.com>
2019-12-18 22:24:34 +0800

    process

--- src/main/resources/static/web/js/lottery.js
+++ src/main/resources/static/web/js/lottery.js
@@ -2,14 +2,53 @@ var angle = 0;
 var rotateIntervalId = null;
 
 function startRotate(){
+    if (rotateIntervalId != null){
+        return
+    }
+    angle = 0;
     rotateIntervalId = setInterval(function(){
-        angle +=3;
+        angle += 15;
         $('#rotate-dish').rotate(angle);
     }, 50);
+
+    var ranInteger = getRndInteger(0,5);
+    setTimeout(function() {
+        showReward(ranInteger);
+    });
 }
 
 function stopRotate(){
      clearInterval(rotateIntervalId);
+     rotateIntervalId = null;
+}
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+function showReward(index){
+     clearInterval(rotateIntervalId);
+     var times = angle/360;
+     if (index == 0){
+        angle = times* 360 + 360;
+     } else if (index == 1){
+        angle = times* 360 + 360 + 323;
+     } else if (index == 2){
+        angle = times* 360 + 360 + 251;
+     }else if (index == 3){
+        angle = times* 360 + 360 + 180;
+     }else if (index == 4){
+        angle = times* 360 + 360 + 107;
+     }else if (index == 5){
+        angle = times* 360 + 360 + 34;
+     }
+
+     $('#rotate-dish').rotate(angle);
+     if (index != 0) {
+        openWin(index);
+     }
+
+     rotateIntervalId = null;
 }
 
 $(document).ready(function(){
